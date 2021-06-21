2f4a9d68816973b6ae92fb18a5b4833441816d2d
KayoRBC <80972278+KayoRBC@users.noreply.github.com>
2021-06-21 01:28:38 -0300

    Add files via upload

---
+++ site/js/funcoesJogo.js
@@ -0,0 +1,83 @@
+// opções inicias jogo
+function mudarPaginaInicial(){
+
+    window.location.href = "../paginasJogo/paginaInicial.html";
+
+}
+
+// rotas da opção 1
+function op1(){
+
+    window.location.href = "../paginasJogo/op1.html";
+
+}
+
+function op1op1(){
+
+    alert("Nesse exato momento o leão do Proerd estava passando do seu lado, e você foi preso. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op1op2(){
+
+    window.location.href = "../paginasJogo/op1op2.html";
+}
+
+function op1op2op1(){
+
+    alert("A vida não é um GTA meu caro, você foi preso. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op1op2op2(){
+
+    alert("Você não sabia que ela praticava jiu-jitsu há 50 anos, F. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op1op2op3(){
+
+    alert("Você estava despreparado por ser um morador de rua. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+// rotas opção 2
+function op2(){
+
+    window.location.href = "../paginasJogo/op2.html";
+
+}
+
+function op2op1(){
+
+    alert("E assim você nunca mais largou esse emprego até o final de sua vida. Você viveu feliz. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op2op2(){
+
+    window.location.href = "../paginasJogo/op2op2.html";
+}
+
+function op2op2op1(){
+
+    window.location.href = "../paginasJogo/op2op2op1.html";
+}
+
+function op2op2op2(){
+
+    alert("E assim você nunca mais largou esse emprego até o final de sua vida. Você viveu feliz. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op2op2op1op1(){
+
+    alert("A NASA desistiu de suas pesquisas e iniciou um canal do youtube de Minecraft. Após alguns anos você foi preso. FIM DE JOGO")
+    mudarPaginaInicial();
+}
+
+function op2op2op1op2(){
+
+    alert("Você foi preso. Após alguns meses iniciou a Terceira Guerra Mundial. FIM DE JOGO")
+    mudarPaginaInicial();
+}
