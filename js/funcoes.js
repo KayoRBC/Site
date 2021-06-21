37f519f57a2465b1ec509f917be7fdc41ebbd61a
KayoRBC <80972278+KayoRBC@users.noreply.github.com>
2021-06-21 01:31:38 -0300

    Add files via upload

---
+++ js/funcoes.js
@@ -0,0 +1,74 @@
+// mudar para a página de imagens
+function mudaPaginaImagens(){
+
+    window.location.href = "paginas/paginaImagens.html";
+
+}
+
+// mudar para a página de vídeo
+function mudaPaginaVideos(){
+
+    window.location.href = "paginas/paginaVideos.html";
+
+}
+
+// mudar para a páfina de login
+function mudaPaginaLogin(){
+
+    window.location.href = "paginas/paginaLogin.html";
+
+}
+
+// criando variáveis para o login
+var nome;
+var sobrenome;
+var senha;
+var idade;
+var cidade;
+
+// função para enviar os dados do login e ternornar um valor, dependendo do retorno ele vai para a próxima página
+function gravarDados(){
+    // recebendo os inputs
+    nome = document.getElementById("nome").value;
+    sobrenome = document.getElementById("sobrenome").value;
+    senha = document.getElementById("senha").value;
+    idade = document.getElementById("idade").value;
+    cidade = document.getElementById("cidade").value;
+
+    // verificando se todos os campos estão preenchidos
+    if (nome != '' && sobrenome != '' && senha != '' && idade != '' && cidade != ''){
+        
+        // função para enviar os dados para o servidor
+        comunicaServidor();
+
+    }
+    else{
+
+        alert("Todos os campos devem ser preenchidos!")
+
+    }
+}
+
+// função para enviar os dados para o servidor
+function comunicaServidor(){
+    // converteno a senha para hash
+    var senha_hash_md5 = $.MD5(senha);
+    // mandando para o arquivo php e verificando se os inputs coincidem com o cadastro
+    $.ajax({
+        type:"POST",
+        dataType:"json",
+        url:"../php/gravarDados.php",
+        data: {ch_nome:nome, ch_sobrenome:sobrenome, ch_senha:senha_hash_md5, ch_idade:idade, ch_cidade:cidade},
+        success: function(resultado){
+            // caso o retorno for "cadastrado", ele irá mudar para a próxima página 
+            if (resultado == "Cadastrado"){
+                window.location.href = "../paginas/paginaJogo.html";
+            }
+            // caso o retorno for "não cadastrado", o site irá avisar que os inputs são inválios (não cadastrado)
+            else {
+                alert("Nao cadastrado");
+            }
+
+        }
+    })
+}
---
+++ php/gravarDados.php
@@ -0,0 +1,26 @@
+<?php
+	// receber os dados dos inputs do login
+	$nome = $_POST["ch_nome"];
+	$sobrenome = $_POST["ch_sobrenome"];
+	$senha = $_POST["ch_senha"];
+	$idade = $_POST["ch_idade"];
+	$cidade = $_POST["ch_cidade"];
+
+	$texto = file_get_contents("../musica.txt");
+
+	$usuario = $texto[6] . $texto[3] . $texto[6] . $texto[3] . $texto[65] . $texto[16];
+
+	// verificar se os inputs correspondem ao cadastro
+	// caso os inputs correspondam ao cadastro, retorna "Cadastrado"
+	if($nome == $usuario && $sobrenome == "cabeludo" && $senha == "202cb962ac59075b964b07152d234b70" && $idade == "69" && $cidade == "felpslandia"){
+		$retorno = utf8_encode("Cadastrado");
+		$json = json_encode($retorno);
+		echo $json;
+	}
+	// caso os inputs não correspondam ao cadastro, retorna "Não cadastrado"
+	else{
+		$retorno = utf8_encode("Nao cadastrado");
+		$json = json_encode("$retorno");
+		echo $json;
+	}
+?>
